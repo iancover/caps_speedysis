c14617cac33c631736746f2d8988d11659894c99
Ian Cover <iancover@gmail.com>
2017-07-11 12:27:55 -0500

    no big changes yet

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+
+
+//  PageSpeed Insights:
+// 		overview: https://developers.google.com/speed/docs/insights/about
+//		info returned:
+//				score - 0-100 'speed' and 'usability'
+//				rule - 
+
+// parameters: 
+//			prettyprint: true - returns reponse in human readable format
+//			url: string - use to fetch and analyze
+//			strategy: string (acceptable values: 'desktop' or 'mobile')
+
+// paths: 
+// 		for speed and usability scores:
+//					data.usability.score & data.speed.score
+//		
+//
+
+//	API INFO:
+//  	endpoint: https://www.googleapis.com/pagespeedonline/v1/
+//		key: AIzaSyD01Sfue8NJalRTGWxfP5UpjpqBoCW2LG4
+// 		
+
+
+
+// FUNCTION FLOW:
 //  1. watch form submit
 //  2. get value from input form / store in variable / reset form ...run get data from api
 //  3. get data from api
+//  4. render results into html template
+//  5. display results
+
+
+
+
+
+
+
+
+// 
