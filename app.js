aaf5413c09c92981fdbae75af862c2bf526f4b8a
Ian Cover <iancover@gmail.com>
2017-07-26 14:40:54 -0500

    deleted unwanted files

--- app.js
+++ app.js
@@ -50,12 +50,17 @@ function renderSuggestions(results) {
 	for (var i in ruleResults) {
 		var eachResult = ruleResults[i];
 		console.log(eachResult);
+
+		// Need to iterate the links to go with each item...
+
 		if (eachResult.ruleImpact < 3.0) continue;
-				suggestResults.push({
-					name: eachResult.localizedRuleName,
-					impact: eachResult.ruleImpact,
-				});	
-			}
+		suggestResults.push({
+			name: eachResult.localizedRuleName,
+			impact: eachResult.ruleImpact,
+		});	
+		}
+	}
+
 	// console.log(suggestResults);
 	suggestResults.sort(function(a,b) {
 	  	return b.impact - a.impact;
@@ -66,87 +71,19 @@ function renderSuggestions(results) {
 	for (var i = 0; i < suggestResults.length; ++i) {
 		var impactScoreLong = suggestResults[i].impact;
 		var impactScoreFixed = impactScoreLong.toFixed(2); 
+		var link = 
 		// console.log(suggestResults[i]);
 		// if summary option...goes here	
 		var resultListItem = (
 			'<tr>' +
 				'<td> Rule </td>' +
-				'<td><a class="suggested-link" target="_blank">' + suggestResults[i].name + '</td>' +
+				'<td><a href="' + varX + '" target="_blank">' + suggestResults[i].name + '</td>' +
 				'<td>Impact Score: <span class="js-impact-score">' + impactScoreFixed + '</span></td><br>'  +
 			'</tr>'
 			);
 
 		console.log(suggestResults);
 
-		if (ruleResults == 'AvoidLandingPageRedirects') {
-			console.log('AvoidLandingPageRedirects');
-			var linkA = 'https://developers.google.com/speed/docs/insights/AvoidRedirects';
-			resultListItem.find('.suggested-link').addAttr('href', linkA);
-			// path: suggestResults.AvoidLandingPageRedirects.summary.args.0.value
-		}
-
-		if (ruleResults == 'EnableGzipCompression') {
-			console.log('EnableGzipCompression');
-			var linkB = 'https://developers.google.com/speed/docs/insights/EnableCompression';
-			resultListItem.find('.suggested-link').addAttr('href', linkB);
-			// path: suggestResults.EnableGzipCompression.summary.args.0.value
-		}
-
-		if (ruleResults == 'LeverageBrowserCaching') {
-			console.log('LeverageBrowserCaching');
-			var linkC = 'https://developers.google.com/speed/docs/insights/LeverageBrowserCaching';
-			resultListItem.find('.suggested-link').addAttr('href', linkC);
-			// path: suggestResults.LeverageBrowserCaching.urlBlocks.0.header.args.0.value
-		}
-
-		if (ruleResults == 'MainResourceServerResponseTime') {
-			console.log('MainResourceServerResponseTime');
-			var linkD = 'https://developers.google.com/speed/docs/insights/Server';
-			resultListItem.find('.suggested-link').addAttr('href', linkD);
-			// path: suggestResults.MainResourceServerResponseTime.urlBlocks.0.header.args.1.value
-		}
-
-		if (ruleResults == 'MinifyCss') {
-			console.log('MinifyCss');
-			var linkE = 'https://developers.google.com/speed/docs/insights/MinifyResources';
-			resultListItem.find('.suggested-link').addAttr('href', linkE);
-			// path: suggestResults.MinifyCss.summary.args.0.value
-		}
-
-		if (ruleResults == 'MinifyHTML') {
-			console.log('MinifyHTML');
-			var linkF = 'https://developers.google.com/speed/docs/insights/MinifyResources';
-			resultListItem.find('.suggested-link').addAttr('href', linkF);
-			// path: suggestResults.MinifyHTML.urlBlocks.0.header.args.0.value
-		}
-
-		if (ruleResults == 'MinifyJavaScript') {
-			console.log('MinifyJavaScript');
-			var linkG = 'https://developers.google.com/speed/docs/insights/MinifyResources';
-			resultListItem.find('.suggested-link').addAttr('href', linkG);
-			// path: suggestResults.MinifyJavaScript.summary.args.0.value 
-		}
-
-		if (ruleResults == 'MinimizeRenderBlockingResources') {
-			console.log('MinimizeRenderBlockingResources');
-			var linkH = 'https://developers.google.com/speed/docs/insights/BlockingJS';
-			resultListItem.find('.suggested-link').addAttr('href', linkH);
-			// path: suggestResults.MinimizeRenderBlockingResources.urlBlocks.1.args.0.value
-		}
-
-		if (ruleResults == 'suggestResults.OptimizeImages') {
-			console.log('OptimizeImages');
-			var linkI = 'https://developers.google.com/speed/docs/insights/OptimizeImages';
-			resultListItem.find('.suggested-link').addAttr('href', linkI);
-			// path: suggestResults.OptimizeImages.urlBlocks.0.header.args.0.value
-		}
-
-		if (ruleResults == 'PrioritizeVisibleContent') {
-			console.log('PrioritizeVisibleContent');
-			var linkJ= 'https://developers.google.com/speed/docs/insights/PrioritizeVisibleContent';
-			resultListItem.find('.suggested-link').addAttr('href', linkJ);
-			// path: suggestResults.PrioritizeVisibleContent.urlBlocks.0.header.args.0.value
-		}
 
 		listTempInsert.append(resultListItem);
 	}
